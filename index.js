f492e3a5880830c7443b9a7c5578dcf47ede6832
Susan <suzutoyuki@gmail.com>
2018-01-20 09:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ function sayHiToGrandma(string){
   }
   else if (string===string.toUpperCase()){
     return "YES INDEED!"
+  }else if (string=== "I love you, Grandma."){
+    return "I love you, too."
   }
 }
